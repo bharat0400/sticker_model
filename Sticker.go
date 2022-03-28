50317e9332c8d0f065354d1826cc749eef03b412
bharat0400 <101325863+bharat0400@users.noreply.github.com>
2022-03-28 16:04:42 +0530

    Add files via upload

---
+++ Sticker.go
@@ -0,0 +1,13 @@
+package sticker_model
+
+type Sticker struct {
+	id           int
+	name         string
+	priority     int
+	start_hour   int
+	end_hour     int
+	published_at string
+	created_at   string
+	updated_at   string
+	url          string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bharat0400/sticker_model
+
+go 1.17
